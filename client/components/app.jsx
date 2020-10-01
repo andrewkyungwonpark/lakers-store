027258323b32d75d4f534a35a6d1f1315e521049
andrewkyungwonpark <andrewkpark@yahoo.com>
2020-09-30 17:57:22 -0700

    User can add to cart - front end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,9 +10,12 @@ export default class App extends React.Component {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.getCartItems = this.getCartItems.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   setView(name, params) {
@@ -24,13 +27,44 @@ export default class App extends React.Component {
     });
   }
 
+  getCartItems() {
+    fetch('/api/cart')
+      .then(results => results.json())
+      .then(items => {
+        this.setState(state => ({
+          cart: this.state.cart.concat(items)
+        }));
+      });
+  }
+
+  addToCart(product) {
+    const post = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    };
+    fetch('./api/cart', post)
+      .then(results => results.json())
+      .then(item => {
+        this.setState(state => ({
+          cart: this.state.cart.concat(item)
+        }));
+      });
+  }
+
+  componentDidMount() {
+    this.getCartItems();
+  }
+
   render() {
     return (
       <div className="container">
-        <Header text="Wicked Sales" />
+        <Header text="Wicked Sales" cartItemCount={this.state.cart.length} />
         {this.state.view.name === 'catalog'
           ? <ProductList setView={this.setView} />
-          : <ProductDetails setView={this.setView} params={this.state.view.params}/>
+          : <ProductDetails setView={this.setView} params={this.state.view.params} addToCart={this.addToCart}/>
         }
       </div>
     );
